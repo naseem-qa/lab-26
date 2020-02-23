e4d5faae0ba1af0969e8655aa10d7be7042d9f59
naseem <izzatnaseem30@gmail.com>
2020-02-23 17:21:52 +0200

    lab26

---
+++ naseem/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Header from './modules/Header.js'
+import Footer from './modules/Footer.js'
+import Main from './modules/Main.js'
+import './app.scss';
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Header />
+        <Main />
+        <Footer />
+      </React.Fragment>
+    )
+  }
+}
+
+export default App;
---
+++ naseem/src/modules/Footer.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+      <footer>first footer</footer>
+    )
+  }
+  export default Footer;
